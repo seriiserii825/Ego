83dddd1adeae066db8bee878f6ffce07c155bf76
serii <seriiburduja@gmail.com>
2017-02-22 19:48:25 +0200

    finish index.html

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -5,7 +5,7 @@ $(function () {
 		$(document).ready(function () {
 				var location = window.location.href;
 				var cur_url = '' + location.split('/').pop();
-				$('#js-main-menu a').each(function () {
+				$('.js-main-menu a').each(function () {
 						var link = $(this).attr('href');
 						if(cur_url == link){
 								$('.js-main-menu__item').removeClass('active');
@@ -15,8 +15,11 @@ $(function () {
 		});
 		
     /*show main menu*/
-		$('#js-show-main-menu').on('click', function () {
-				$('#js-main-menu').slideToggle();
+		$('.js-show-main-menu').on('click', function () {
+				var current = $(this).attr('data-menu');
+				console.log(current);
+
+				$('#' + current).slideToggle();
 		});
     
 		/*slick.js*/
