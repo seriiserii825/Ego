21d03713927b47d0798273d236171e8d2221d98a
serii <seriiburduja@gmail.com>
2017-02-22 02:32:47 +0200

    finish main-header and main-slider and started main-footer

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -1,5 +1,34 @@
 $(function () {
+
+		/*choose menu item
+		 =========================================================*/
+		$(document).ready(function () {
+				var location = window.location.href;
+				var cur_url = '' + location.split('/').pop();
+				$('#js-main-menu a').each(function () {
+						var link = $(this).attr('href');
+						if(cur_url == link){
+								$('.js-main-menu__item').removeClass('active');
+								$(this).addClass('active');
+						}
+				});
+		});
+		
+    /*show main menu*/
+		$('#js-show-main-menu').on('click', function () {
+				$('#js-main-menu').slideToggle();
+		});
     
-    
+		/*slick.js*/
+		$('#js-main-slider').slick({
+				arrows: false,
+				dots: true,
+				fade: true,
+				speed: 4000,
+				autoplay: true,
+				autoplaySpeed: 4000
+
+
+		});
     
 });
